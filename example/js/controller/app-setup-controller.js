cc9223596108747244e2ea641d6a88dab34d26ff
Manan <manan.bharara@gmail.com>
2013-08-18 15:44:48 +0530

    Adding console logging to example

--- example/js/controller/app-setup-controller.js
+++ example/js/controller/app-setup-controller.js
@@ -1,5 +1,9 @@
 function AppSetupController(IndexedDB) {
   this.save = function () {
-    IndexedDB.put("sample", this.sample);
+    IndexedDB.put("sample", this.sample, function () {
+      console.log("Updated!")
+    }, function () {
+      console.log("Failed");
+    });
   }
 }
